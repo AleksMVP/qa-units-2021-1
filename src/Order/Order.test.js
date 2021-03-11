8754692d2a087c02fdb90b918fbcfb00fe243f57
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-03-11 20:48:20 +0300

    Add some test

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import Order from './Order';
+import {getDate} from '../utils/getDate';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {fakeOrders} from '../data/fakeOrders';
+
+jest.mock('../utils/getDate');
+getDate.mockReturnValue("15 марта, чт, 2021 год");
+
+configure({ adapter: new Adapter() });
 
 describe('Order.js', () => {
-  it('some test', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  afterAll(() => {
+    jest.resetModules();
+  });
 
+  it('Snapshot test', () => {
+    const order = fakeOrders[1];
+    const wrapper = shallow(<Order 
+      order={order}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Bad object', () => {
+    const wrapper = shallow(<Order 
+      bober={{}}
+    />);
+    expect(wrapper.getElement()).toEqual(null);
+  });
+
+  it('Bad object', () => {
+    const order = fakeOrders[0];
+    const wrapper = shallow(<Order 
+      order={{}}
+    />);
+
+    expect(wrapper.getElement()).toEqual(null);
   });
 });
 
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,24 +1,135 @@
 import React from 'react'
 import {sortByItemCount} from './sortOrders';
+import {sortByDate} from './sortOrders';
+import {getSortFunction} from './sortOrders';
+import {sortOrders} from './sortOrders';
+import {sortTypes} from './sortOrders';
 
 describe('sortByItemCount function', () => {
-	it('orders are null', () => {
-		const result = sortByItemCount(null, null);
-		expect(result).toEqual(0);
+	it('First lenght is bigger', () => {
+		const first = {
+			items: ['one', 'two'],
+		};
+
+		const second = {
+			items: ['ont', 'two', 'three'],
+		};
+
+		const result = sortByItemCount(first, second);
+
+		expect(result).toEqual(-1);
 	});
 
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
+	it('Second length is bigger', () => {
+		const first = {
+			items: ['one', 'two', 'three'],
 		};
 
-		const order2 = {
-			items: ['1', '2'],
+		const second = {
+			items: ['ont', 'two'],
 		};
 
-		const result = sortByItemCount(order1, order2);
+		const result = sortByItemCount(first, second);
 
-		expect(result).toBe(0);
+		expect(result).toEqual(1);
+	});
+
+	test.each([
+		[null, null, 0],
+		[1, 2, 0],
+		[{items: []}, {items: []}, 0],
+		[{items: ['item1', 'item2']}, {items: ['1', '2']}, 0],
+		[{values: ['somevalue']}, {values: ['somevalue']}, 0]
+	])('Zero output', (a, b, expected) => {
+		const result = sortByItemCount(a, b);
+		expect(result).toBe(expected);
+	});
+});
+
+describe('sortByDate function', () => {
+	it('Second date is bigger', () => {
+		const first = {
+			date: new Date("1995-12-17T03:24:00"),
+		};
+
+		const second = {
+			date: new Date("1996-12-17T03:24:00"),
+		};
+
+		const result = sortByDate(first, second);
+
+		expect(result).toEqual(1);
+	});
+
+	it('First date is bigger', () => {
+		const first = {
+			date: new Date("1996-12-17T03:24:00"),
+		};
+
+		const second = {
+			date: new Date("1991-11-17T03:24:00"),
+		};
+
+		const result = sortByDate(first, second);
+
+		expect(result).toEqual(-1);
+	});
+
+	
+	test.each([
+		[null, null, 0],
+		[1, 2, 0],
+		[{items: []}, {items: []}, 0],
+		[{date: new Date("1996-12-17T03:24:00")}, {date: new Date("1996-12-17T03:24:00")}, 0],
+		[{time: new Date("1996-12-17T03:24:00")}, {time: new Date("1996-12-17T03:24:00")}, 0]
+	])('Zero output', (a, b, expected) => {
+		const result = sortByDate(a, b);
+		expect(result).toBe(expected);
+	});
+});
+
+describe('getSortFunction function', () => {
+	it('Sort by item count', () => {
+		const result = getSortFunction(sortTypes.COUNT);
+
+		expect(result).toEqual(sortByItemCount);
+	});
+
+	it('Sort by date', () => {
+		const result = getSortFunction(sortTypes.DATE);
+
+		expect(result).toEqual(sortByDate);
+	});
+
+	it('Return undefined', () => {
+		const result = getSortFunction(1);
+
+		expect(result).toEqual(undefined);  // != 
+	});
+});
+
+describe('SortOrders function', () => {
+	it('Sort DESC', () => {
+		const desc_input = [
+			{date: new Date("1990-12-17T03:24:00")},
+			{date: new Date("1996-12-17T03:24:00")},
+		];
+
+		const acs_input = [
+			{date: new Date("1996-12-17T03:24:00")},
+			{date: new Date("1990-12-17T03:24:00")},
+		];
+
+		const expected = [
+			{date: new Date("1996-12-17T03:24:00")},
+			{date: new Date("1990-12-17T03:24:00")},
+		];
+
+		sortOrders(acs_input, sortByDate);
+		sortOrders(desc_input, sortByDate);
+
+		expect(acs_input).toEqual(expected);
+		expect(desc_input).toEqual(expected);
 	});
 });
 
